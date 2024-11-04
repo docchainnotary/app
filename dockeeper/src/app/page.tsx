2ea8cac1a161f6e60c217745e9e85fa34871df2e
Christopher Robison <cdr@netoasis.net>
2024-11-04 13:21:44 -0800

    Initial updates

---
+++ dockeeper/src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import DocumentNotaryApp from './components/dockeeper-app';
+
+export default function Home() {
+  return (
+    <main>
+      <DocumentNotaryApp />
+    </main>
+  );
+}
